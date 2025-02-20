9fd4fef74a7fc7fd81f5bfee5444d765bd445c35
Racheal <rachealnjugush@gmail.com>
2025-02-20 16:14:19 +0300

    search bar

--- shopIT-Frontend/src/components/Navbar.jsx
+++ shopIT-Frontend/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import {Link} from 'react-router-dom'
+import {FaHome, FaInfoCircle, FaEnvelope} from 'react-icons/fa'
+import Search from './Search'
 function Navbar(){
     return(
         <>
             <nav id='navbar'>
-                <Link id='nav-link' to={'/'}>Home</Link>
-                <Link id='nav-link' to={'/about'}>About</Link>
-                <Link id='nav-link' to={'/contact'}>Contact</Link>
+
+                <Link id='nav-link' to={'/'}><FaHome size={20}/> Home</Link>
+                <Link id='nav-link' to={'/about'}><FaInfoCircle size={20}/> About</Link>
+                <Link id='nav-link' to={'/contact'}><FaEnvelope size={20}/> Contact</Link>
+                <div id='search'>
+                    <Search />
+                </div>
             </nav>
         </>
     )
