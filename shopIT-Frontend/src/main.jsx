5a507397c07af8fc07c02d437ea5f915386ae842
Racheal <rachealnjugush@gmail.com>
2025-02-20 12:10:21 +0300

    created components and added route code to main.jsx file

--- shopIT-Frontend/src/main.jsx
+++ shopIT-Frontend/src/main.jsx
@@ -2,6 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import home from './Home'
+import about from './About'
+import contact from './Contact'
+import signup from './Signup'
+import login from './Login'
+
+import {createBrowserResource, RouteProvider} from 'react-route-dom' 
+
+const route = createBrowserResource([
+  {
+    path: '/',
+    element: '<Layout />',
+    children: [
+      {
+        path: '/home',
+        element: '<Home />'
+      },
+      {
+        path: '/about',
+        element: '<About />'
+      },
+      {
+        path: '/contact',
+        element: '<Contact />'
+      },
+      {
+        path: '/signup',
+        element: '<Signup />'
+      },
+      {
+        path: '/login',
+        element: '<Login />'
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
