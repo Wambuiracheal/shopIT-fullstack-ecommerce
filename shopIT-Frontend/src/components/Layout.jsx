ae74845cc4e57f7dd8c6b0b6a7e8b65787ec8758
Racheal <rachealnjugush@gmail.com>
2025-02-20 15:19:25 +0300

    styled the navbar

--- shopIT-Frontend/src/components/Layout.jsx
+++ shopIT-Frontend/src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import {Outlet} from 'react-router-dom'
 
-const Layout{
-  return (
-    <div>
-      
-    </div>
+function Layout(){
+  return(
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </>
   )
 }
 
