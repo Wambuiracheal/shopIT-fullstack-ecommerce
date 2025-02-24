04265fc87debbb14cac808e28b5ff13625f970e5
Racheal <rachealnjugush@gmail.com>
2025-02-24 04:50:03 +0300

    added cart to navbar

--- shopIT-Frontend/src/components/Navbar.jsx
+++ shopIT-Frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom'
-import {FaHome, FaInfoCircle, FaEnvelope} from 'react-icons/fa'
+import {FaHome, FaInfoCircle, FaEnvelope,  FaShoppingCart } from 'react-icons/fa'
 import Search from './Search'
 import logo from '../assets/logo.png'
 function Navbar(){
@@ -12,6 +12,7 @@ function Navbar(){
                 <Link id='nav-link' to={'/'}><FaHome size={20}/> Home</Link>
                 <Link id='nav-link' to={'/about'}><FaInfoCircle size={20}/> About</Link>
                 <Link id='nav-link' to={'/contact'}><FaEnvelope size={20}/> Contact</Link>
+                <Link id='nav-link' to={'/cart'}><FaShoppingCart  size={20}/> Cart</Link>
                 <div id='search'>
                     <Search />
                 </div>
