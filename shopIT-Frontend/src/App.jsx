0d2a72cd6da9ef5a2f2dcb07ed899c5c72ce2016
josh704 <joshuangetich02@gmail.com>
2025-02-24 10:47:09 +0300

    initial commit

--- shopIT-Frontend/src/App.jsx
+++ shopIT-Frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import SellerDashboard from "./pages/SellerDashboard";
+import BuyerDashboard from "./pages/BuyerDashboard";
 
-function App(){
-  return(
-    <>
-   
-    </>
-  )
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} /> 
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/seller-dashboard" element={<SellerDashboard />} />
+        <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
