22b0459f222ec891008c3708ce073aaa0e55d2aa
Racheal Wambui Njuguna <rachealnjugush@gmail.com>
2025-03-11 00:14:48 +0300

    work on mpesa

--- shopIT-Frontend/src/components/Mpesa.jsx
+++ shopIT-Frontend/src/components/Mpesa.jsx
@@ -59,7 +59,7 @@ function MpesaPayment() {
 
   return (
     <div className="container">
-      <h2 className="title">Marksmatt</h2>
+      <h2 className="title">shopIT</h2>
       <p className="subtitle">
         Pay with <span className="mpesa-text">M<span className="hyphen">-</span>pesa</span>
       </p>
